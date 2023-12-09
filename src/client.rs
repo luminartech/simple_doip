35fd48131f32d1300b126623566b89dc744988b8
Zach <zheylmun@gmail.com>
2023-12-09 11:42:57 -0500

    not available on all platforms

--- src/client.rs
+++ src/client.rs
@@ -66,7 +66,6 @@ impl DoIPClient {
             SocketAddr::V6(_) => TcpSocket::new_v6()?,
         };
         tcp_socket.set_reuseaddr(true)?;
-        tcp_socket.set_reuseport(true)?;
         const BUFFER_SIZE: u32 = 1024 * 1024 * 4;
         tcp_socket.set_recv_buffer_size(BUFFER_SIZE)?;
         tcp_socket.set_send_buffer_size(BUFFER_SIZE)?;
