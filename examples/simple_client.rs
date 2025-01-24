3d8650005e3a7a2656ec4a80d329cb43e03dd59c
Zach <zheylmun@gmail.com>
2025-01-24 12:59:08 -0500

    example client- update example client

--- examples/simple_client.rs
+++ examples/simple_client.rs
@@ -2,21 +2,22 @@ use std::net::{IpAddr, SocketAddr};
 
 use doip::{
     client::{DoIPClient, DoIPClientOptions},
-    messages::{header::ProtocolVersion, routing_activation_request::ActivationTypeCode},
-    server::SERVER_TCP_PORT,
+    messages::{ActivationTypeCode, ProtocolVersion},
+    TCP_PORT,
 };
+use uds_protocol::{ProtocolRequest, ProtocolResponse};
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     let options = DoIPClientOptions {
-        server_address: SocketAddr::from(([127, 0, 0, 1], SERVER_TCP_PORT)),
+        server_address: SocketAddr::from(([127, 0, 0, 1], TCP_PORT)),
         server_logical_address: 0xE400,
         server_physical_address: 0x4010,
         client_address: IpAddr::from([0, 0, 0, 0]),
         client_logical_address: 0x0E01,
         protocol_version: ProtocolVersion::V2012,
     };
-    let mut client = DoIPClient::connect(options).await?;
+    let mut client = DoIPClient::<ProtocolRequest, ProtocolResponse>::connect(options).await?;
     client
         .request_routing_activation(ActivationTypeCode::Default, None)
         .await?;
